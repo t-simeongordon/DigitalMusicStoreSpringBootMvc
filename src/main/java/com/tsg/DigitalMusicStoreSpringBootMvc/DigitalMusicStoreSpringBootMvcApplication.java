96ec5ba127059e2fe3a5dbcb942ace011eff414e
t-simeon-gordon <terrel.simeon-gordon@accenture.com>
2020-11-25 00:43:23 +0000

    V1 base created

---
+++ src/main/java/com/tsg/DigitalMusicStoreSpringBootMvc/DigitalMusicStoreSpringBootMvcApplication.java
@@ -0,0 +1,11 @@
+package com.tsg.DigitalMusicStoreSpringBootMvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalMusicStoreSpringBootMvcApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalMusicStoreSpringBootMvcApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tsg/DigitalMusicStoreSpringBootMvc/DigitalMusicStoreSpringBootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsg.DigitalMusicStoreSpringBootMvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalMusicStoreSpringBootMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
