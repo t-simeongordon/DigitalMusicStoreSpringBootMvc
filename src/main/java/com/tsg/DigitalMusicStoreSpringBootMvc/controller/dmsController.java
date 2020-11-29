c5cd04f9941d0ee93222454e475b8288d270f089
t-simeon-gordon <terrel.simeon-gordon@accenture.com>
2020-11-29 21:26:24 +0000

    V1 exposed new endpoint topchart, populated genre list
    Signed-off-by: t-simeon-gordon <terrel.simeon-gordon@accenture.com>

--- src/main/java/com/tsg/DigitalMusicStoreSpringBootMvc/controller/dmsController.java
+++ src/main/java/com/tsg/DigitalMusicStoreSpringBootMvc/controller/dmsController.java
@@ -5,6 +5,7 @@ import com.tsg.DigitalMusicStoreSpringBootMvc.services.MoodAndGenreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class dmsController {
     @GetMapping("/genre")
     public ResponseEntity<List<Genre>> getAllMusicGenres() {
         return ResponseEntity.ok().body(moodAndGenreService.getMoodAndGenreList());
+    }
+
+    @GetMapping("/topchart")
+    public ResponseEntity<String> getTopCharts() {
+        return ResponseEntity.ok().body("k");
     }
 }
