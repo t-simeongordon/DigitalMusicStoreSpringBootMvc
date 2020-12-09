4c24b7cdabd089f57523ec03ea175a6cc2050323
t-simeon-gordon <terrel.simeon-gordon@accenture.com>
2020-12-09 05:01:19 +0000

    V0 added request mapping to properties file
    Signed-off-by: t-simeon-gordon <terrel.simeon-gordon@accenture.com>

--- src/main/java/com/tsg/DigitalMusicStoreSpringBootMvc/controller/dmsController.java
+++ src/main/java/com/tsg/DigitalMusicStoreSpringBootMvc/controller/dmsController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v0/music")
 public class dmsController {
     @Autowired
     MusicContentService musicContentService;
